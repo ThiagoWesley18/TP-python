cfc67c1da7a5ede215632f0399cc79cf2e3ca994
ThiagoWesley18 <tw180896@gmail.com>
2024-06-07 20:17:01 -0400

    implementaçao da função em tempo real

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 import matplotlib
 import pandas as pd
@@ -10,17 +11,28 @@ from matplotlib.backends.backend_tkagg import (
     NavigationToolbar2Tk
 )
 
-
 class App(tk.Tk):
     def __init__(self, dados):
         super().__init__()
 
         self.title('Desemprego IBGE')
+        self.dados = dados
+        self.last_modified = os.path.getmtime('dadosBD.csv') # Pega o tempo da última modificação do arquivo
+        self.after(1000, self.check_file) # Verifica se o arquivo foi modificado a cada 1 segundo e chama o método check_file
+        self.create_widgets() # Cria os widgets da aplicação
 
+    # Método para limpar os widgets da aplicação
+    def clear_widgets(self):
+        # Destroi todos os widgets filhos do frame principal da aplicação
+        for widget in self.winfo_children():
+            widget.destroy()
+
+    # Método para criar os widgets da aplicação
+    def create_widgets(self):
         # Cria um dicionário com os dados que serão plotados
         data = {
-            "Taxa de desemprego": dados.iloc[0,0],
-            "Taxa de Subtilização": dados.iloc[0,3]
+            "Taxa de desemprego": self.dados.iloc[0,0],
+            "Taxa de Subtilização": self.dados.iloc[0,3]
         }
         indices = data.keys()
         valores = data.values()
@@ -49,8 +61,8 @@ class App(tk.Tk):
         frame = tk.Frame(self)
         frame.pack()
 
-        total_rows = dados.shape[0] 
-        total_columns = dados.shape[1] 
+        total_rows = self.dados.shape[0] 
+        total_columns = self.dados.shape[1] 
         column_labels = ["Desempregados (Milhões)", "Taxa de Desemprego (%)", "Ocupados (Milhões)", "Taxa de Subtilização (%)"]
 
         # Cria os rótulos das colunas
@@ -63,8 +75,17 @@ class App(tk.Tk):
             for j in range(total_columns): 
                 e = tk.Entry(frame, width=20, fg='blue', font=('Arial',10)) 
                 e.grid(row=i+1, column=j)  
-                e.insert(tk.END, str(dados.iloc[i,j])) 
-
+                e.insert(tk.END, str(self.dados.iloc[i,j])) 
+    
+    # Método para verificar se o arquivo foi modificado
+    def check_file(self):
+        current_time = os.path.getmtime('dadosBD.csv') # Pega o tempo da última modificação do arquivo
+        if current_time != self.last_modified:
+            self.last_modified = current_time
+            self.dados = pd.read_csv('dadosBD.csv')
+            self.clear_widgets() # Limpa os widgets da aplicação
+            self.create_widgets() # Cria os widgets da aplicação
+        self.after(1000, self.check_file) # Chama o método check_file a cada 1 segundo 
 
 if __name__ == '__main__':
     # Criando um DataFrame com os dados do arquivo teste.csv
